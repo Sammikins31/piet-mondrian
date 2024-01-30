11a321b5da88488ebc771185951abc4ce24960bb
Sammikins31 <samantha.chan@student.norwichuni.ac.uk>
2024-01-30 14:19:17 +0000

    update piet colour grid

--- sketch.js
+++ sketch.js
@@ -14,30 +14,33 @@ function setup() {
     const cellNumber = 10;
     //  canvas size in pixels
     const canvasSize = squareSize * cellNumber;
+    
+    // set width and height
+    const width = squareSize;
+    const height = squareSize;
 
     createCanvas(canvasSize, canvasSize);
     background(220);
 
-    for(let row = 0; row < cellNumber; row++) {
-        for(let column = 0; column < cellNumber; column++) {
+    let x = 0;
+    let y = 0;
 
+    while(y < canvasSize) {
+        
+        x = 0;
 
+        while(x < canvasSize) {
+      
+
+            // choose random hex code from all the possible colours
             const colorCode = random(possibleCololrs);
 
+            // store colour object in myColor
             const myColor = color(colorCode);
 
             // use fill to fill all the square with one colour
             fill(myColor)
 
-            // calculate horizontal position in pixels
-            const x = row * squareSize;
-            // calculate the vertical position in pixels
-            const y = column * squareSize;
-
-
-            // set width and height
-            const width = squareSize;
-            const height = squareSize;
 
             // rect(x, y, width, height)
             // x = position of the left of the square
@@ -47,6 +50,11 @@ function setup() {
             // square(x, y, squareSize);
           rect(x, y, width, height);
 
+        //   update x
+        x = x + width;
+
         }
+        // update y
+        y = y + height;
     }
 }
